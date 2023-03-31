e72bf14cd4bbe0bc28499a8ba2b14cf8b34afd45
Dragon44588 <105646122+Dragon44588@users.noreply.github.com>
2023-03-31 14:04:47 +1100

    finished database read write

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ if __name__ == "__main__": # apparently this is cool
     testMessage = "Hello world!"
 
     readDatabase()
+    x = {"Previous Hash":"Gen", "Data":"10", "Proof of Work": 5,"Correction Hash":"50"}
+    writeMainDatabase(x)
     #Function testing shenanigans
     testChain = standardChain()
     print(testChain)
